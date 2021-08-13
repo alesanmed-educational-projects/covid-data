01048ae5fd7c9bbcd96ad90db814846539a831c1
Ale Sanchez <alesanchezmedina@gmail.com>
2021-08-13 07:42:26 +0200

    fix: Removed comments

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ with open(os.path.join(os.path.dirname(__file__), "pyproject.toml"), "r") as f:
 download_url = "https://github.com/alesanmed-educational-projects/covid-data/archive/refs/tags/{}.tar.gz".format(__version__)
 
 setup(
-  name = 'covid_data',         # How you named your package folder (MyLib)
-  packages = ['covid_data'],   # Chose the same as "name"
-  version = __version__,      # Start with a small number and increase it with every change you make
-  license='The Unlicense',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Data loader part of the mid-project for the Data Science bootcamp from Core Code School',   # Give a short description about your library
-  author = 'alesanchez',                   # Type in your name
-  author_email = 'hi@alesanchez.es',      # Type in your E-Mail
-  url = 'https://github.com/alesanmed-educational-projects/core-data-covid-project',   # Provide either the link to your github or to your website
-  download_url = download_url,    # I explain this later on
-  keywords = ['covid', 'core'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'covid_data',
+  packages = ['covid_data'],
+  version = __version__,
+  license='The Unlicense',
+  description = 'Data loader part of the mid-project for the Data Science bootcamp from Core Code School',
+  author = 'alesanchez',
+  author_email = 'hi@alesanchez.es',
+  url = 'https://github.com/alesanmed-educational-projects/core-data-covid-project',
+  download_url = download_url,
+  keywords = ['covid', 'core'],
+  install_requires=[
       'pandas',
       'beautifulsoup4',
       'click',
@@ -32,10 +32,10 @@ setup(
       'Unidecode',
   ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Scientific/Engineering :: Information Analysis',
-    'License :: OSI Approved :: The Unlicense (Unlicense)',   # Again, pick a license
+    'License :: OSI Approved :: The Unlicense (Unlicense)',
     'Programming Language :: Python :: 3.9',
   ],
 )
