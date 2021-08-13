f15af43de37562d9c78d1901fcf09e810a32da28
Ale Sanchez <alesanchezmedina@gmail.com>
2021-08-13 07:49:10 +0200

    fix: Fixed formatting

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import os
 import re
 from distutils.core import setup
 
-with open('README.md', 'r', encoding='utf-8') as fp:
-	long_description = fp.read()
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
 
 with open(os.path.join(os.path.dirname(__file__), "pyproject.toml"), "r") as f:
     project_info = f.read()
@@ -12,34 +12,36 @@ with open(os.path.join(os.path.dirname(__file__), "pyproject.toml"), "r") as f:
 
     __version__ = re.findall(regex, project_info)[0]
 
-download_url = "https://github.com/alesanmed-educational-projects/covid-data/archive/refs/tags/{}.tar.gz".format(__version__)
+download_url = "https://github.com/alesanmed-educational-projects/covid-data/archive/refs/tags/{}.tar.gz".format(
+    __version__
+)
 
 setup(
-  name = 'covid_data',
-  packages = ['covid_data'],
-  version = __version__,
-  license='The Unlicense',
-  description = 'Data loader part of the mid-project for the Data Science bootcamp from Core Code School',
-  long_description=long_description,
-  author = 'alesanchez',
-  author_email = 'hi@alesanchez.es',
-  url = 'https://github.com/alesanmed-educational-projects/covid-data',
-  download_url = download_url,
-  keywords = ['covid', 'core'],
-  install_requires=[
-      'pandas',
-      'beautifulsoup4',
-      'click',
-      'python',
-      'requests',
-      'psycopg2',
-      'Unidecode',
-  ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Information Analysis',
-    'License :: OSI Approved :: The Unlicense (Unlicense)',
-    'Programming Language :: Python :: 3.9',
-  ],
+    name="covid_data",
+    packages=["covid_data"],
+    version=__version__,
+    license="The Unlicense",
+    description="Data loader part of the mid-project for the Data Science bootcamp from Core Code School",
+    long_description=long_description,
+    author="alesanchez",
+    author_email="hi@alesanchez.es",
+    url="https://github.com/alesanmed-educational-projects/covid-data",
+    download_url=download_url,
+    keywords=["covid", "core"],
+    install_requires=[
+        "pandas",
+        "beautifulsoup4",
+        "click",
+        "python",
+        "requests",
+        "psycopg2",
+        "Unidecode",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
